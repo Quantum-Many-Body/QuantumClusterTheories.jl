b321c1fd79288536e188482f9296543a8c04bd6b
Documenter.jl <documenter@juliadocs.github.io>
2022-05-07 09:08:15 +0000

    build based on 7e3b62e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuantumClusterTheory","category":"page"},{"location":"#QuantumClusterTheory","page":"Home","title":"QuantumClusterTheory","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuantumClusterTheory.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuantumClusterTheory]","category":"page"}]
+}
