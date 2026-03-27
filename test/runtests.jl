eaa6befcb64966e79f24ccdb400128baf519cd52
waltergu <waltergu1989@gmail.com>
2026-03-27 21:21:26 +0800

    Add Plots dependency and spectral function test
    - Add Plots to test Project.toml dependencies
    - Import LinearAlgebra.tr in tests
    - Add Square-Hubbard-Spectral test with CPT spectral function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 using ExactDiagonalization
-using LinearAlgebra: I, inv
+using LinearAlgebra: I, inv, tr
 using QuantumClusterTheories
 using QuantumLattices
 using Test
 using TightBindingApproximation
+import Plots
 
 @testset "QuantumClusterTheories" begin
     unitcell = Lattice([0.0, 0.0]; vectors=[[1.0, 0.0], [0.0, 1.0]])
@@ -53,4 +54,28 @@ using TightBindingApproximation
 
     cpt = CPT(unitcell, lattice, hilbert, terms, quantumnumber)
     @test cpt(ω, k) ≈ inv(ω*I-[2cos(k[1])+2cos(k[2]) 0; 0 2cos(k[1])+2cos(k[2])])
+end
+
+@testset begin "Square-Hubbard-Spectral"
+    unitcell = Lattice([0.0, 0.0]; vectors=[[1.0, 0.0], [0.0, 1.0]])
+    lattice = Lattice(unitcell, (2, 2), ('P', 'P'))
+    hilbert = Hilbert(site=>Fock{:f}(1, 2) for site in eachindex(lattice))
+    t = Hopping(:t, -1.0, 1)
+    U = Hubbard(:U, 8.0)
+    μ = Onsite(:μ, -U.value/2)
+    quantumnumber = ℕ(length(lattice)) ⊠ 𝕊ᶻ(0)
+    cpt = CPT(unitcell, lattice, hilbert, (t, μ, U), quantumnumber)
+    emin = -10.0
+    emax = 10.0
+    N = 501
+    η = 0.1
+    es = LinRange(emin, emax, N)
+    path = ReciprocalPath(reciprocals(unitcell), rectangle"Γ-X-M-Γ"; length=100)
+    data = zeros(length(es), length(path))
+    for (i, e) in enumerate(es)
+        for (j, k) in enumerate(path)
+            data[i, j] = -2*tr(imag(cpt(e+1im*η, k)))
+        end
+    end
+    Plots.savefig(Plots.plot(path, es, data), "Plots-Hubbard-Square-2x2-spectral.png")
 end
