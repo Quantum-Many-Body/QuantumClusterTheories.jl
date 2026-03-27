7581a62dd4ba2ba8cde346551b083c5d9795616c
waltergu <waltergu1989@gmail.com>
2026-03-27 09:42:26 +0800

    Add CPT periodization, operators function, and refactor EDSolver
    - Fix Periodization to handle empty vectors case properly
    - Add operators function for TBA and BdG kinds to extract single-particle operators
    - Add quadratic helper function to extract rank-2 terms from Term composite
    - Refactor EDSolver to use BandLanczosMethod and RetardedGreenFunction
    - Add CPT constructor with lattice, hilbert, terms, and quantumnumbers

--- ext/QuantumClusterTheoriesExactDiagonalizationExt.jl
+++ ext/QuantumClusterTheoriesExactDiagonalizationExt.jl
@@ -1,29 +1,49 @@
 module QuantumClusterTheoriesExactDiagonalizationExt
 
-using ExactDiagonalization: ED, Algorithm, GreenFunction, RetardedGreenFunction
-using ExactDiagonalization: QuantumOperator, ExactMethod, prepare!
-using QuantumClusterTheories: CPT, ImpuritySolver
+using ExactDiagonalization: Abelian, BandLanczosMethod, ED, EDKind, EDMatrixization, RetardedGreenFunction
+using QuantumLattices: AbstractLattice, Hilbert, Metric, Neighbors, OneOrMore, QuantumOperator, Table, Term, nneighbor, atol, eager, plain, rtol
+using QuantumClusterTheories: ImpuritySolver, Periodization, operators, perturbation, quadratic 
+using TightBindingApproximation: TBAKind
 
 """
-    EDSolver{O<:Number, G<:Union{GreenFunction{O}, RetardedGreenFunction{O}}} <: ImpuritySolver
-
-An impurity solver based on exact diagonalization that wraps an ED algorithm and a GreenFunction.
 """
-struct EDSolver{O<:Number, G<:Union{GreenFunction{O}, RetardedGreenFunction{O}}} <: ImpuritySolver
-    ed::Algorithm{<:ED}
-    greenfunction::G
+struct EDSolver{E<:ED, O<:QuantumOperator, G<:RetardedGreenFunction} <: ImpuritySolver
+    ed::E
+    operators::Vector{O}
+    gf::G
 end
-@inline (solver::EDSolver)(ω::Number) = solver.greenfunction(ω)
+@inline (solver::EDSolver)(ω::Number) = solver.gf(ω)
 
 """
-    EDSolver(ed::Algorithm{<:ED}, operators::AbstractVector{<:QuantumOperator}, method=ExactMethod(); kwargs...)
-
-Construct an `EDSolver` from an ED algorithm and a list of operators.
 """
-function EDSolver(ed::Algorithm{<:ED}, operators::AbstractVector{<:QuantumOperator}, method=ExactMethod(); kwargs...)
-    prepare!(ed)
-    greenfunction = RetardedGreenFunction(operators, ed, method; kwargs...)
-    return EDSolver(ed, greenfunction)
+@inline function EDSolver(ed::ED, operators::AbstractVector{<:QuantumOperator}, method=BandLanczosMethod(); kwargs...)
+    return EDSolver(ed, operators, RetardedGreenFunction(operators, ed, method; kwargs...))
+end
+function EDSolver(
+    lattice::AbstractLattice, hilbert::Hilbert, terms::OneOrMore{Term}, quantumnumbers::OneOrMore{Abelian}, method=BandLanczosMethod(), dtype::Type{<:Number}=valtype(terms);
+    neighbors::Union{Int, Neighbors}=nneighbor(terms), kwargs...
+)
+    system = Generator(filter!(isintracell, bonds(lattice, neighbors)), hilbert, OneOrMore(terms), plain, eager; half=false)
+    edkind = EDKind(hilbert)
+    table = Table(hilbert, Metric(edkind, hilbert))
+    sectors = broadcast(Sector, OneOrMore(quantumnumbers), hilbert; table)
+    matrixization = EDMatrixization{dtype}(table, sectors...)
+    ed = ED{typeof(edkind)}(lattice, system, matrixization)
+    tbakind = TBAKind(typeof(terms), valtype(hilbert))
+    ops = operators(tbakind, lattice, hilbert, Table(hilbert, Metric(tbakind, hilbert)))
+    return EDSolver(ed, ops, method; kwargs...)
+end
+
+"""
+"""
+function CPT(
+    unitcell::AbstractLattice, lattice::AbstractLattice, hilbert::Hilbert, terms::OneOrMore{Term}, quantumnumbers::OneOrMore{Abelian}, method=BandLanczosMethod(), dtype::Type{<:Number}=valtype(terms);
+    neighbors::Union{Int, Neighbors}=nneighbor(terms), atol=atol, rtol=rtol, kwargs...
+)
+    solver = EDSolver(lattice, hilbert, terms, quantumnumbers, method, dtype; neighbors)
+    pert = perturbation(lattice, hilbert, terms; neighbors)
+    periodization = Periodization([rcoordinate(op) for op in solver.operators], unitcell.vectors; atol, rtol)
+    return CPT(unitcell, lattice, solver, pert, periodization)
 end
 
 end # module
--- src/QuantumClusterTheories.jl
+++ src/QuantumClusterTheories.jl
@@ -1,10 +1,13 @@
 module QuantumClusterTheories
 
 using LinearAlgebra: inv
-using QuantumLattices: AbstractLattice, Frontend, Generator, Hilbert, Metric, Neighbors, OneOrMore, Term, bonds, isintracell, issubordinate, lazy, matrix, nneighbor, plain
+using QuantumLattices: AbstractLattice, Fock, Frontend, Generator, Hilbert, Metric, Neighbors, OneAtLeast, OneOrMore, Table, Term
+using QuantumLattices: atol, bonds, isannihilation, isintracell, issubordinate, lazy, matrix, nneighbor, plain, rank, rtol
 using StaticArrays: SVector
 using TightBindingApproximation: Quadraticization, TBA, TBAKind
 
+"""
+"""
 abstract type ImpuritySolver end
 
 """
@@ -13,18 +16,20 @@ struct Periodization{N}
     coordinates::Vector{SVector{N, Float64}}
     groups::Vector{Vector{Int}}
 end
-function Periodization(coordinates::AbstractVector{<:AbstractVector{<:Number}}, vectors::AbstractVector{<:AbstractVector{<:Number}}; atol, rtol)
+function Periodization(coordinates::AbstractVector{<:AbstractVector{<:Number}}, vectors::AbstractVector{<:AbstractVector{<:Number}}; atol=atol, rtol=rtol)
     groups = Vector{Int}[]
     indexes = collect(eachindex(coordinates))
     while !isempty(indexes)
         i = popfirst!(indexes)
         coordinateᵢ = coordinates[i]
         group = [i]
-        for j in indexes
-            coordinateⱼ = coordinates[j]
-            issubordinate(coordinateᵢ-coordinateⱼ, vectors; atol, rtol) && push!(group, j)
+        if length(vectors)>0
+            for j in indexes
+                coordinateⱼ = coordinates[j]
+                issubordinate(coordinateᵢ-coordinateⱼ, vectors; atol, rtol) && push!(group, j)
+            end
+            filter!(∉(group), indexes)
         end
-        filter!(∉(group), indexes)
         push!(groups, group)
     end
     return Periodization(coordinates, groups)
@@ -57,17 +62,32 @@ struct CPT{L<:AbstractLattice, I<:ImpuritySolver, T<:TBA, P<:Periodization} <: F
     periodization::P
 end
 
+"""
+"""
+@inline (cpt::CPT)(ω::Number) = inv(inv(cpt.solver(ω))-matrix(cpt.perturbation))
+@inline (cpt::CPT)(ω::Number, k::AbstractVector{<:Number}) = cpt.periodization(inv(inv(cpt.solver(ω))-matrix(cpt.perturbation, k)), k)
+
+"""
+"""
+function operators(::TBAKind{:TBA}, lattice::AbstractLattice, hilbert::Hilbert{<:Fock}, table::Table)
+    result = [CoordinatedIndex(Index(site, fockindex), coordinate, zero(coordinate)) for (site, coordinate) in enumerate(lattice) for fockindex in hilbert[site] if isannihilation(fockindex)]
+    return sort!(result; by=index->table[index])
+end
+function operators(::TBAKind{:BdG}, lattice::AbstractLattice, hilbert::Hilbert{<:Fock}, table::Table)
+    result = [CoordinatedIndex(Index(site, fockindex), coordinate, zero(coordinate)) for (site, coordinate) in enumerate(lattice) for fockindex in hilbert[site]]
+    return sort!(result; by=index->table[index])
+end
+
+"""
+"""
 function perturbation(lattice::AbstractLattice, hilbert::Hilbert, terms::OneOrMore{Term}; neighbors::Union{Int, Neighbors}=nneighbor(terms))
+    terms = quadratic(OneOrMore(terms))
     K = typeof(TBAKind(typeof(terms), valtype(hilbert)))
     H = Generator(filter!(!isintracell, bonds(lattice, neighbors)), hilbert, terms, plain, lazy; half=false)
     quadraticization = Quadraticization{K}(Table(hilbert, Metric(K(), hilbert)))
     commt = commutator(K(), hilbert)
     return TBA{K}(lattice, H, quadraticization, commt)
 end
-
-"""
-"""
-@inline (cpt::CPT)(ω::Number) = inv(inv(cpt.solver(ω))-matrix(cpt.perturbation))
-@inline (cpt::CPT)(ω::Number, k::AbstractVector{<:Number}) = cpt.periodization(inv(inv(cpt.solver(ω))-matrix(cpt.perturbation, k)), k)
+@generated quadratic(terms::OneAtLeast{Term}) = Expr(:tuple, [:(terms[$i]) for (i, T) in enumerate(fieldtypes(terms)) if rank(T)==2]...)
 
 end
